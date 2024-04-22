b8ee2f595005b8fa952bba7330d5f536e9933ea8
tdevaharsha <167551732+tdevaharsha@users.noreply.github.com>
2024-04-22 22:19:56 +0530

    Add files via upload

---
+++ DHT_Physics.py
@@ -0,0 +1,56 @@
+NCERT Class 11
+
+Physics Chapter 1: Units and Measurements
+
+Important points
+
+-There are three system of units: CGS, FPS, MKS
+
+-SI units and quantities
+Base quantity          Name           Symbol
+Length                 Metre           m
+Mass                   Kilogram        kg
+Time                   Second          s
+Electricity            Ampere          A
+Thermodynamic          Kelvin          K
+temperature
+Amount of subtance     Mole            mol
+Luminous intensity     Candela         cd
+
+-The reliable digits plus the first uncertain digit are known as significant digits or significant figures.
+
+-Rules of significant figures:
+• All the non-zero digits are significant.
+• All the zeros between two non-zero digits are significant, no matter where the decimal point is, if at all.
+• If the number is less than 1, the zeros on the right of decimal point but to the left of the first non-zero digit are
+not significant.
+• The terminal or trailing zero(s) in a number without a decimal point are not significant.
+• The trailing zero(s) in a number with a decimal point are significant.
+
+-Rules for operations of significant figures:
+(1)In multiplication or division, the final result should retain as many significant figures as are there in the
+original number with the least significant figures.
+(2)In addition or subtraction, the final result should retain as many decimal places as are there in the number
+with the least decimal places.
+
+-Errors
+
+-Let a measurement bet taken carefully n times, if these measured values are a1, a2, a3....an, then their arithematic
+mean is considered the most accurate value.
+
+-If a set of experimental data is specified to n significant figures, a result obtained by combining the data will also
+be valid to n significant figures.
+
+-Accuracy measures closeness to the true value, while precision indicates consistency.
+
+-Dimensions
+
+The dimensions of a physical quantity are the powers (or exponents) to which the base quantities are raised to represent
+that quantity.
+
+All the physical quantities can be written in terms of the dimensions [L], [M] and [T].
+
+The magnitudes of physical quantities may be added together or subtracted from one another only if they have the same
+dimensions.
+
+
